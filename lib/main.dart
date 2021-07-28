0f59b68f6180807a8e25f80576b124f17b57da17
Ahmed Elsayed <a.elsayed@studio50s.com>
2021-07-28 14:17:02 +0200

    Removing unneeded cubit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_cubit_bloc_tutorial/pages/weather_search_page.dart';
 import 'package:flutter_cubit_bloc_tutorial/weather_bloc.dart';
-import 'package:flutter_cubit_bloc_tutorial/weather_cubit.dart';
 
 import 'data/weather_repository.dart';
 void main() => runApp(MyApp());
